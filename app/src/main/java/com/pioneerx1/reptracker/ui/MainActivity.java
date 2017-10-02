0dadd2ec722030aa35a0d7c2b65776951c44e51e
Mick Sexton <lacrookedbeat@hotmail.com>
2017-10-02 08:26:23 -0700

    add initial files and folder structure

---
+++ app/src/main/java/com/pioneerx1/reptracker/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pioneerx1.reptracker.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pioneerx1.reptracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
