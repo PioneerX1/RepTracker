03e6ef9a65c20947a413f4877caf36c20bf01a2e
Mick Sexton <lacrookedbeat@hotmail.com>
2017-10-02 14:07:27 -0700

    add successful API call to ProPublica, brings back json data in the logcat

--- app/src/main/java/com/pioneerx1/reptracker/ui/SearchCongressActivity.java
+++ app/src/main/java/com/pioneerx1/reptracker/ui/SearchCongressActivity.java
@@ -8,10 +8,15 @@ import android.support.v7.widget.RecyclerView;
 import com.pioneerx1.reptracker.R;
 import com.pioneerx1.reptracker.adapters.RepListAdapter;
 import com.pioneerx1.reptracker.models.Rep;
+import com.pioneerx1.reptracker.services.ProPublicaService;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.Bind;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class SearchCongressActivity extends AppCompatActivity {
 
@@ -22,6 +27,9 @@ public class SearchCongressActivity extends AppCompatActivity {
 
     @Bind(R.id.congressRepsRecyclerView) RecyclerView mCongressRepsRecyclerView;
 
+    // hard-code this for now:
+    String congressChamber = "senate";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +37,28 @@ public class SearchCongressActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_congress);
 
         Log.d("You have arrived at ", " SEARCH CONGRESS ACTIVITY");
+        getCongressMembers(congressChamber);
     }
+
+    private void getCongressMembers(String congressChamber) {
+        final ProPublicaService propublicaService = new ProPublicaService();
+        propublicaService.findCongressMembers(congressChamber, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //e.printStackTrace();
+                Log.d("FAILURE ", " INVOKED---");
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.d("API RESPONSE---", jsonData);
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                    Log.d("CANNOT READ ", "JSON RESPONSE");
+                }
+            }
+        });
+    }
+
 }
