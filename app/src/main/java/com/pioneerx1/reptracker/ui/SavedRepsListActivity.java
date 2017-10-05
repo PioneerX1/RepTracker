e7d38bd0a938c97ef10bc5007833eac0d48eac2d
Mick Sexton <lacrookedbeat@hotmail.com>
2017-10-05 13:38:19 -0700

    add SavedRepsListActivity to utilize rep_list_item and FirebaseRepViewHolder to render the reps that have been saved from firebase

--- app/src/main/java/com/pioneerx1/reptracker/ui/SavedRepsListActivity.java
+++ app/src/main/java/com/pioneerx1/reptracker/ui/SavedRepsListActivity.java
@@ -2,16 +2,59 @@ package com.pioneerx1.reptracker.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.pioneerx1.reptracker.Constants;
 import com.pioneerx1.reptracker.R;
+import com.pioneerx1.reptracker.adapters.FirebaseRepViewHolder;
+import com.pioneerx1.reptracker.models.Rep;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SavedRepsListActivity extends AppCompatActivity {
+
+    private DatabaseReference mRepReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.congressRepsRecyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_reps_list);
+        setContentView(R.layout.activity_search_congress);  // re-using search congress layout
         Log.d("You have arrived at ", " SAVED REPS LIST ACTIVITY");
+        ButterKnife.bind(this);
+
+        mRepReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_SAVED_MEMBERS);
+        setUpFirebaseAdapter();
     }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Rep, FirebaseRepViewHolder>
+                (Rep.class, R.layout.rep_list_item, FirebaseRepViewHolder.class, mRepReference) {
+
+            @Override
+            protected void populateViewHolder(FirebaseRepViewHolder viewHolder, Rep model, int position) {
+                viewHolder.bindRep(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+
 }
